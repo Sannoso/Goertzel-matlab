256fb41aaf86ed89baf9f4b0a052fa2524b21b57
Sannoso <sanderruben@live.nl>
2016-02-02 13:57:48 +0100

    calculated DFT indices

--- goertzel.m
+++ goertzel.m
@@ -17,3 +17,22 @@ hfg = [1209 1336 1477];  % High frequency group
 
 % Generate 12 frequency pairs
 frequencypairs   = [reshape(ones(3,1)*lfg,1,12); repmat(hfg,1,4)];
+
+% The minimum duration of a DTMF signal defined by the ITU standard is 40
+% ms. Therefore, there are at most 0.04 x 8000 = 320 samples available for
+% estimation and detection. The DTMF decoder needs to estimate the
+% frequencies contained in these short signals. 
+%
+% One common approach to this estimation problem is to compute the
+% Discrete-Time Fourier Transform (DFT) samples close to the seven
+% fundamental tones. For a DFT-based solution, it has been shown that using
+% 205 samples in the frequency domain minimizes the error between the
+% original frequencies and the points at which the DFT is estimated.
+Nsamples = 205;
+original_frequencies = [lfg(:);hfg(:)];  % Original frequencies
+
+k = round((original_frequencies/Fs)*Nsamples);  % Indices of the DFT
+estim_f = round(k*Fs/Nsamples);      % Frequencies at which the DFT is estimated
+
+
+stem(sounddata)
