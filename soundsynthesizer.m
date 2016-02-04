6b8500c565276ee5627c245d6aba065fdae337b4
Sannoso <sanderruben@live.nl>
2016-02-04 18:53:15 +0100

    made soundsamples to verify my Goertzel results

---
+++ soundsynthesizer.m
@@ -0,0 +1,13 @@
+%synthesyze sounds to analyse with the goertzel algorithm.
+clear all;
+
+symbol = {'1','2','3','4','5','6','7','8','9','*','0','#'};
+[tones, Fs, f, lfg, hfg] = helperDTMFToneGenerator(symbol, false);
+tones = tones/2; %scale all vectors to have min and max -1 ;+1
+%stem(tones(:,1))
+
+
+audiowrite('tone_1.wav',tones(:,1),8000);
+[sounddata,Fsound] = audioread('tone_1.wav');
+
+stem(sounddata)
