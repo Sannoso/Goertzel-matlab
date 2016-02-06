bcdbeb3bd64d693054f6e36f8376a6a5341de394
Sannoso <sanderruben@live.nl>
2016-02-06 21:13:44 +0100

    added categorization of inputsounds

--- dtmf_goertzel.m
+++ dtmf_goertzel.m
@@ -9,6 +9,7 @@
 
 clear all;
 
+%%
 % Read in the sound data
 for toneChoice=1:12,
     filename = strcat('sounds\tone_',num2str(toneChoice),'.wav');
@@ -17,7 +18,7 @@ for toneChoice=1:12,
 
 
     Fs  = 8000;       % Sampling frequency 8 kHz
-    disp(strcat('Soundinput ', filename))
+    disp(strcat('Soundinput :', filename))
     if Fsound == Fs
         disp(' has the correct sampling frequency')
     else
@@ -28,11 +29,10 @@ for toneChoice=1:12,
 
 end
 
+%%
+%setting all DTMF and Goertzel parameters
 lfg = [697 770 852 941]; % Low frequency group
 hfg = [1209 1336 1477];  % High frequency group
-
-% Generate 12 frequency pairs
-frequencypairs   = [reshape(ones(3,1)*lfg,1,12); repmat(hfg,1,4)];
 
 % The minimum duration of a DTMF signal defined by the ITU standard is 40
 % ms. Therefore, there are at most 0.04 x 8000 = 320 samples available for
@@ -50,6 +50,8 @@ original_frequencies = [lfg(:);hfg(:)];  % Original frequencies
 k = round((original_frequencies/Fs)*Nsamples);  % Indices of the DFT
 estim_f = round(k*Fs/Nsamples);      % Frequencies at which the DFT is estimated
 
+%%
+%doing frequency analysis on input
 %{
 %debugging
 stem(sounddata)
@@ -63,7 +65,6 @@ sounddata = sounddata(2200: 2200+Nsamples);
 %stem(sampledata)
 %pause
 %}
-%sounddata(1:205)
 
 for inputChoice=1:12,
     %goertzel analysis on each tone
@@ -80,3 +81,43 @@ for inputChoice=1:12,
     title(strcat('DFT Magnitudes tone nr: ', num2str(inputChoice)))
 
 end
+
+%%
+%categorization of DFT data to a symbol
+
+% Generate 12 frequency pairs
+frequencypairs   = [1,1,1,2,2,2,3,3,3,4,4,4;5,6,7,5,6,7,5,6,7,5,6,7];
+symbol = {'1','2','3','4','5','6','7','8','9','*','0','#'};
+
+%check for existing frequencies
+dft_threshold = 40;
+categorization = zeros(7,12);
+for i=1:7
+    for j = 1:12
+        if(abs(dft_data(i,j)) > dft_threshold)
+            categorization(i,j) = 1; % frequency is present
+        end
+    end
+end
+
+%%{
+temp = zeros(2,7);
+for j=1:12
+    counter = 0;
+    for i=1:7
+        if(categorization(i,j)==1)
+            counter = counter +1;
+            if(counter >2) %check wether there are more then two frequencies present
+                disp(strcat('too many frequencies present in signal : ',num2str(j)))
+            else
+                temp(counter,j) = i;
+            end
+        end
+    end
+    for a=1:12
+        if(temp(:,j) == frequencypairs(:,a))
+            disp(strcat('sounds nr: ', num2str(j), ' is the DTMF sound of the symbol :"', symbol(j),'" '))
+        end
+    end
+end
+ %}           
