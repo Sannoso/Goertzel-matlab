c7d7fd31200b9651e8651e58f8c2a80798d9e6f5
Sannoso <sanderruben@live.nl>
2016-02-06 16:12:03 +0100

    finished script to create all 12 DTMF tones

--- soundsynthesizer.m
+++ soundsynthesizer.m
@@ -3,15 +3,27 @@ clear all;
 
 symbol = {'1','2','3','4','5','6','7','8','9','*','0','#'};
 [tones, Fs, f, lfg, hfg] = helperDTMFToneGenerator(symbol, false);
-%tones = tones/2; %scale all vectors to have min and max -1 ;+1
+tones = tones(1:205,:); %we only need 205 samples, so truncate
+tones = tones/2; %scale all vectors to have min and max -1 ;+1
 
-%%{
+%{
 %debug
 stem(tones(:,1))
 pause
 %}
 
-audiowrite('tone_1.wav',tones(:,1),8000);
-[sounddata,Fsound] = audioread('tone_1.wav');
+for toneChoice=1:12,
+    % Select tone
+    tone=tones(:,toneChoice);
+    filename = strcat('tone_',num2str(toneChoice),'.wav');
+    audiowrite(filename, tone, 8000);
+    %{
+    %debug
+    filename
+    [sounddata, Fsound] = audioread(filename);
+    stem(sounddata)
+    pause
+    %}
+end
 
-stem(sounddata)
+
